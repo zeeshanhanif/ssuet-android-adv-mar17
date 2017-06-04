f14fe8521dbe51c8ba851a085f1fd6e06d50388b
ishaquehassan <ishaquehassan@gmail.com>
2017-06-04 13:33:49 +0500

    Class work of 4th June 2017

---
+++ 4June2017/AppOne/app/src/main/java/com/company/appone/Main22Activity.java
@@ -0,0 +1,13 @@
+package com.company.appone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main22Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main22);
+    }
+}
---
+++ 4June2017/AppOne/app/src/main/java/com/company/appone/MainActivity.java
@@ -0,0 +1,17 @@
+package com.company.appone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b = new Button(this);
+        b.setText("Button One");
+
+    }
+}
